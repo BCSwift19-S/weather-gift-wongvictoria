b27735648b5bef1878ba14f2a6a7d8afb549e04c
Victoria Wong <VictoriaWong@victorias-macbook-air.local>
2019-03-11 01:14:42 -0400

    7.3 Completed

--- WeatherGift/PageVC.swift
+++ WeatherGift/PageVC.swift
@@ -9,22 +9,53 @@
 import UIKit
 
 class PageVC: UIPageViewController {
-
+    
+    var currentPage = 0
+    var locationsArray = ["Local City", "Sydney, Australia","Accra, Ghana", "Uglich, Russia"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        delegate = self
+        dataSource = self
+        
+        setViewControllers([createDetailedVC(forPage: 0)], direction: .forward, animated: false, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func createDetailedVC(forPage page: Int) -> DetailedVC {
+        
+        currentPage = min(max(0, page), locationsArray.count - 1)
+        
+        let detailedVC = storyboard!.instantiateViewController(withIdentifier: "DetailedVC") as! DetailedVC
+        
+        detailedVC.locationsArray = locationsArray
+        detailedVC.currentPage = currentPage
+        
+        return detailedVC
     }
-    */
+}
+
+
+extension PageVC: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        if let currentViewController = viewController as? DetailedVC {
+            if currentViewController.currentPage < locationsArray.count - 1 {
+                return createDetailedVC(forPage: currentViewController.currentPage + 1)
+            }
+        }
+    
+        return nil
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        if let currentViewController = viewController as? DetailedVC {
+            if currentViewController.currentPage > 0 {
+                return createDetailedVC(forPage: currentViewController.currentPage - 1)
+            }
+        }
+        
+        return nil
+    }
 
 }
