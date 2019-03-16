69bc6a649a5722f04c82404c787dd473c4c25249
Victoria Wong <VictoriaWong@victorias-macbook-air.local>
2019-03-16 18:18:28 -0400

    7.9 Completed

--- WeatherGift/AppDelegate.swift
+++ WeatherGift/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        GMSPlacesClient.provideAPIKey("AIzaSyBY9a7f6RH-N8AKDD_h7f7GniZ6LlaY06U") 
         return true
     }
 
